05bf905875ddf0ad8ac225c0ff084b433c97db86
Rupert Caingal <johnrupertcaingal@gmail.com>
2025-08-13 13:46:32 +0800

    feat: user crud

---
+++ src/services/telegramAccountService.ts
@@ -0,0 +1,37 @@
+import type { TelegramAccount } from "../../generated/prisma";
+import prisma from "../config/prisma";
+
+export const create = async (account: Omit<TelegramAccount, "id">) => {
+  return prisma.telegramAccount.create({
+    data: account,
+  });
+};
+
+export const getById = async (id: string) => {
+  return prisma.telegramAccount.findUnique({
+    where: { id },
+  });
+};
+
+export const update = async (id: string, data: Partial<TelegramAccount>) => {
+  return prisma.telegramAccount.update({
+    where: { id },
+    data,
+  });
+};
+
+export const getByTelegramId = async (telegramId: string) => {
+  return prisma.telegramAccount.findUnique({
+    where: { telegramId },
+  });
+};
+
+export const getByUserId = async (userId: string) => {
+  return prisma.telegramAccount.findFirst({
+  where: {
+    user: {
+      id: userId
+    }
+  }
+});
+};
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,9 +1,48 @@
-// import { AppDataSource } from "../config/dataSource";
-// import { User } from "../models/user";
+import type { User } from "../../generated/prisma";
+import prisma from "../config/prisma";
 
-// const userRepo = AppDataSource.getRepository(User)
+export const create = async (user: Omit<User, "id">) => {
+  return prisma.user.create({
+    data: user,
+  });
+};
 
-// export const create = async (user: Omit<User, "_id">)=>{
-//     const newUser = userRepo.create(user)
-//     return await userRepo.save(newUser); 
-// }
+export const getById = async (id: string) => {
+  return prisma.user.findUnique({
+    where: { id },
+    include: {
+      telegramAccount:true,
+      discordAccount:true 
+    }
+    
+  });
+};
+
+export const update = async (id: string, data: Partial<User>) => {
+  return prisma.user.update({
+    where: { id },
+    data,
+  });
+};
+
+export const getByTgId = async (telegramId: string) => {
+  return prisma.user.findFirst({
+    where: {
+      telegramAccount: {
+        telegramId,
+      },
+    },
+    include: { telegramAccount: true, discordAccount:true },
+  });
+};
+
+export const getByDiscordId = async (discordId: string) => {
+  return prisma.user.findFirst({
+    where: {
+      discordAccount: {
+        discordId,
+      },
+    },
+    include: { discordAccount: true, telegramAccount: true, },
+  });
+};
